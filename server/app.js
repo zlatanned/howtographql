95bab309a48f6106858ccfee4a7fcdb3f4b55da3
Akshay Shahi <shahiakshay31@gmail.com>
2020-11-15 18:54:29 +0530

    fix  URLSearchParams error due to node version <10

--- server/app.js
+++ server/app.js
@@ -2,6 +2,11 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/index');
 
+//to fix URLSearchParams not defined error for node version < v10.0.0
+//not required if node -v >= 10
+const { URLSearchParams } = require('url');
+global.URLSearchParams = URLSearchParams;
+
 const app = express();
 
 // bind express with graphql
