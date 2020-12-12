707828a0acd0e9bcd162a4b7c9cd9836adfec028
Akshay Shahi <shahiakshay31@gmail.com>
2020-12-13 01:39:13 +0530

    removed dummy data ahead of mutations

--- server/schema/index.js
+++ server/schema/index.js
@@ -10,23 +10,6 @@ const { GraphQLObjectType,
     GraphQLInt,
     GraphQLList } = graphql;
 
-// dummy data
-// const players = [
-//     { name: 'Lionel Messi', nationality: 'Argentina', club: 'FC Barcelona', age: 33, id: '1', leagueId: '2' },
-//     { name: 'Cristiano Ronaldo', nationality: 'Portugal', club: 'Juventus', age: 35, id: '2', leagueId: '3' },
-//     { name: 'Eden Hazard', nationality: 'Belgium', club: 'Real Madrid C.F.', age: 29, id: '3', leagueId: '2' },
-//     { name: 'Hakim Ziyech', nationality: 'Morocco', club: 'Chelsea F.C.', age: 27, id: '4', leagueId: '1' },
-//     { name: 'Angel Di Maria', nationality: 'Argentina', club: 'Paris St. Germain', age: 32, id: '5', leagueId: '4' },
-//     { name: 'Kevin De Bruyne', nationality: 'Belgium', club: 'Manchester City', age: 29, id: '6', leagueId: '1' }
-// ];
-
-// const leagues = [
-//     { name: 'Premier League', nation: 'England', id: '1' },
-//     { name: 'La Liga', nation: 'Spain', id: '2' },
-//     { name: 'Serie A', nation: 'Italy', id: '3' },
-//     { name: 'Ligue 1', nation: 'France', id: '4' }
-// ];
-
 const PlayerType = new GraphQLObjectType({
     name: 'Player',
     fields: () => ({
